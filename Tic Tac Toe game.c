194fc61e8f577ba928ecc264d1fdc6b36f85e9dd
Sugumar Sadurshan <sugumarsadurshan@gmail.com>
2025-09-08 00:59:47 +0530

    08th Sep
    08th Sep

--- Tic Tac Toe game.c
+++ Tic Tac Toe game.c
@@ -4,7 +4,7 @@ int main(){
 
     int size;
 
-    printf("Enter the board size N (3 <= N <= 10) : ");
+    printf("Enter the board size N (3 <= size <= 10) : ");
     scanf("%d", &size);
 
     if(size < 3 || size > 10){
@@ -12,17 +12,13 @@ int main(){
         return 1;
     }
 
-int Game_Mode;
+    int Game_Mode;
+
     printf("1. Two Player\n");
     printf("2. Player vs Computer\n");
     printf("3. Multi Player\n");
     printf("Select Game Mode : ");
     scanf("%d", &Game_Mode);
-    
-    if(Game_Mode < 1 || Game_Mode > 3){
-        printf("Invalid game mode. Please select a valid option.\n");
-        return 1;
-    }
 
     if(Game_Mode == 1){
         printf("Starting Two Player mode...\n");
@@ -31,10 +27,14 @@ int Game_Mode;
         printf("Starting Player vs Computer mode...\n");
     
     } else if(Game_Mode == 3){
-        printf("Slect number of players (3-10) : ");
+        printf("Slect number of players : ");
         int numPlayers;
         scanf("%d", &numPlayers);
         printf("Starting Multi Player mode...\n");
+    
+    } else {
+        printf("Invalid game mode. Please select a valid option.\n");
+        return 1;
     }
     return 0;
 }
