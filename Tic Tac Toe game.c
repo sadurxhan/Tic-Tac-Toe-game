cac383d0f884bf9e2646ca2e38048c59600ba35b
Sugumar Sadurshan <sugumarsadurshan@gmail.com>
2025-09-22 00:42:28 +0530

    22 of Sep
    22 of Sep

--- Tic Tac Toe game.c
+++ Tic Tac Toe game.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
 
-void initializeBoard(int n, char b[n][n]);
-void printBoard(int n, char b[n][n]);
-int  getMove(int n, char b[n][n], int *r, int *c, char sym);
-void placeSymbol(int n, char b[n][n], int r, int c, char sym);
-int  inBounds(int n, int r, int c);
-int  isEmpty(int n, char b[n][n], int r, int c);
-void clearStdin(void);
+void initializeBoard(int n, char board[n][n]);
+void printBoard(int n, char board[n][n]);
+int  getMove(int n, char board[n][n], int *r, int *c, char sym);
 
 int main(){
 
@@ -21,11 +17,10 @@ int main(){
     }
 
     char board[size][size];
-
     initializeBoard(size, board);
     printBoard(size, board);
 
-    int Game_Mode;
+    /* int Game_Mode;
 
     printf("1. Two Player\n");
     printf("2. Player vs Computer\n");
@@ -35,7 +30,6 @@ int main(){
 
     if(Game_Mode == 1){
         printf("Starting Two Player mode...\n");
-        void initializeBoard(int size, char board[size][size]);
     
     } else if(Game_Mode == 2){
         printf("Starting Player vs Computer mode...\n");
@@ -49,23 +43,33 @@ int main(){
     } else {
         printf("Invalid game mode. Please select a valid option.\n");
         return 1;
-    }
+    } */
     return 0;
 }
 
-void initializeBoard(int n, char b[n][n]){
+void initializeBoard(int n, char board[n][n]){
     for(int i=0;i<n;i++)
         for(int j=0;j<n;j++)
-            b[i][j] = '_';
+            board[i][j] = '_';
 }
 
-void printBoard(int n, char b[n][n]){
+void printBoard(int n, char board[n][n]){
     printf("\n   ");
     for(int j=0;j<n;j++) printf("%2d", j+1);
     printf("\n");
     for(int i=0;i<n;i++){
         printf("%2d ", i+1);
-        for(int j=0;j<n;j++) printf("%2c", b[i][j]);
+        for(int j=0;j<n;j++) printf("%2c", board[i][j]);
         printf("\n");
     }
+}
+
+int  getMove(int n, char board[n][n], int *r, int *c, char sym){
+        int raw , coloum;
+        printf("Enter your move (row and column) for %c:", sym);
+        scanf("%d %d", &raw, &coloum);
+        if(raw < 1 || raw > n || coloum < 1 || coloum > n || board[raw][coloum] != '_'){
+            printf("Invalid move. Try again.\n");
+            return 1;
+        }
 }
