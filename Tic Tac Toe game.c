43adfca866a5182bd0446f0c9ab812eb4276b277
Sugumar Sadurshan <sugumarsadurshan@gmail.com>
2025-09-23 00:00:57 +0530

    Sep 23
    Sep 23

--- Tic Tac Toe game.c
+++ Tic Tac Toe game.c
@@ -2,36 +2,35 @@
 
 void initializeBoard(int n, char board[n][n]);
 void printBoard(int n, char board[n][n]);
-int  getMove(int n, char board[n][n], int *r, int *c, char sym);
+int getMove(int n, char board[n][n], int *r, int *c, char sym);
 
 int main(){
+    int n;
+    int size = n;
 
-    int size;
-
-    printf("Enter the board size N (3 <= size <= 10) : ");
+    printf("Enter the board size n (3 <= size <= 10) : ");
     scanf("%d", &size);
 
     if(size < 3 || size > 10){
         printf("Invalid board size. Please enter a value between 3 and 10.\n");
         return 1;
     }
-
     char board[size][size];
-    initializeBoard(size, board);
+    initializeBoard(size, board);   
     printBoard(size, board);
 
-    /* int Game_Mode;
+    int Game_Mode;
 
     printf("1. Two Player\n");
-    printf("2. Player vs Computer\n");
-    printf("3. Multi Player\n");
+    //printf("2. Player vs Computer\n");
+    //printf("3. Multi Player\n");
     printf("Select Game Mode : ");
     scanf("%d", &Game_Mode);
 
     if(Game_Mode == 1){
         printf("Starting Two Player mode...\n");
     
-    } else if(Game_Mode == 2){
+    } /* else if(Game_Mode == 2){
         printf("Starting Player vs Computer mode...\n");
     
     } else if(Game_Mode == 3){
@@ -48,20 +47,42 @@ int main(){
 }
 
 void initializeBoard(int n, char board[n][n]){
-    for(int i=0;i<n;i++)
-        for(int j=0;j<n;j++)
-            board[i][j] = '_';
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            board[i][j] = ' ';
+        }
+    }
 }
 
 void printBoard(int n, char board[n][n]){
-    printf("\n   ");
-    for(int j=0;j<n;j++) printf("%2d", j+1);
     printf("\n");
-    for(int i=0;i<n;i++){
-        printf("%2d ", i+1);
-        for(int j=0;j<n;j++) printf("%2c", board[i][j]);
-        printf("\n");
+    printf("   ");
+
+    for(int j = 0; j < n; j++) {
+        printf(" %d ", j + 1);
+        if(j < n - 1) printf(" ");
     }
+    printf("\n");
+    
+    for(int i = 0; i < n; i++) {
+        printf(" %d ", i + 1);
+        
+        for(int j = 0; j < n; j++) {
+            printf(" %c ", board[i][j]);
+            if(j < n - 1) printf("|");
+        }
+        printf("\n");
+        
+        if(i < n - 1) {
+            printf("   ");
+            for(int j = 0; j < n; j++){
+                printf("---");
+                if(j < n - 1) printf("+");
+            }
+            printf("\n");
+        }
+    }
+    printf("\n");
 }
 
 int  getMove(int n, char board[n][n], int *r, int *c, char sym){
