cb999005c58cd0de69790a4b359cc6af0a8a507d
Sugumar Sadurshan <sugumarsadurshan@gmail.com>
2025-09-29 23:50:14 +0530

    Sep 29
    Sep 29

--- Tic Tac Toe game.c
+++ Tic Tac Toe game.c
@@ -4,13 +4,21 @@
 #define Player2 'O'
 #define Player3 'Z'
 
+// Function prototypes
 void initializeBoard(int n, char board[n][n]);
 void printBoard(int n, char board[n][n]);
 int getMove(int n, char board[n][n], int *r, int *c, char sym, int playerNum);
+int validateMove(int n, char board[n][n], int row, int col);
+void makeMove(int n, char board[n][n], int row, int col, char sym);
+int checkWin(int n, char board[n][n], char sym);
+int checkDraw(int n, char board[n][n]);
 
 int main(){
     int size;
     int Game_Mode;
+    int winners = 1;
+    int moves = 0;
+    int currentPlayer = 1;
 
     printf("Enter the board size n (3 <= size <= 10) : ");
     scanf("%d", &size);
@@ -45,6 +53,41 @@ int main(){
         printf("Invalid game mode. Please select a valid option.\n");
         return 1;
     } */
+    
+
+    int row, col;
+    char currentSymbol;
+    int gameOver = 0;
+
+    while(!gameOver){
+        if(currentPlayer == 1) {
+            currentSymbol = Player1;
+        } else {
+            currentSymbol = Player2;
+        }
+
+        int valid = getMove(size, board, &row, &col, currentSymbol, currentPlayer);
+        while(valid != 0){
+            printf("Please try again.\n");
+            valid = getMove(size, board, &row, &col, currentSymbol, currentPlayer);
+        }
+
+        makeMove(size, board, row, col, currentSymbol);
+        printBoard(size, board);
+
+        if(checkWin(size, board, currentSymbol)){
+            printf("Player %d (%c) wins!\n", currentPlayer, currentSymbol);
+            gameOver = 1;
+        }
+        else if(checkDraw(size, board)){
+            printf("It's a draw!\n");
+            gameOver = 1;
+        }
+        else {
+            currentPlayer = (currentPlayer % 2) + 1;
+        }
+    }
+
     return 0;
 }
 
@@ -167,4 +210,14 @@ int checkWin(int n, char board[n][n], char sym){
     if(win) return 1;
 
     return 0;
+}
+int checkDraw(int n, char board[n][n]){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++) {
+            if(board[i][j] == ' ') {
+                return 0; // Found an empty cell, not a draw
+            }
+        }
+    }
+    return 1; // No empty cells, it's a draw
 }
